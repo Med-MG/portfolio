c7557e4a22183a53e876cb75dc61d935946df14d
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2020-09-11 23:48:30 +0100

    js fancybox , slider

--- src/js/main.js
+++ src/js/main.js
@@ -289,3 +289,179 @@ $(function() {
     });
     return false;
   });
+   // masonry Grid
+   $('.art-grid').isotope({
+    filter: '*',
+    itemSelector: '.art-grid-item',
+    transitionDuration: '.6s',
+  });
+
+  // slider testimonials
+  var swiper = new Swiper('.art-testimonial-slider', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    speed: 1400,
+    autoplay: false,
+    autoplaySpeed: 5000,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.art-testi-swiper-next',
+      prevEl: '.art-testi-swiper-prev',
+    },
+    breakpoints: {
+      1500: {
+        slidesPerView: 2,
+      },
+      1200: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 1,
+      },
+    },
+  });
+
+  // slider works
+  var swiper = new Swiper('.art-works-slider', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    speed: 1400,
+    autoplay: {
+      delay: 4000,
+    },
+    autoplaySpeed: 5000,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.art-works-swiper-next',
+      prevEl: '.art-works-swiper-prev',
+    },
+    breakpoints: {
+      1500: {
+        slidesPerView: 2,
+      },
+      1200: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 1,
+      },
+    },
+  });
+
+  // slider blog
+  var swiper = new Swiper('.art-blog-slider', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    speed: 1400,
+    autoplay: {
+      delay: 4000,
+    },
+    autoplaySpeed: 5000,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.art-blog-swiper-next',
+      prevEl: '.art-blog-swiper-prev',
+    },
+    breakpoints: {
+      1500: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 1,
+      },
+    },
+  });
+
+  $('[data-fancybox="gallery"]').fancybox({
+    animationEffect: "zoom-in-out",
+    animationDuration: 600,
+    transitionDuration: 1200,
+    buttons: [
+      "zoom",
+      "share",
+      "slideShow",
+      "thumbs",
+      "close"
+    ],
+  });
+
+  $('[data-fancybox="diplome"]').fancybox({
+    animationEffect: "zoom-in-out",
+    animationDuration: 600,
+    transitionDuration: 1200,
+    buttons: [
+      "zoom",
+      "slideShow",
+      "thumbs",
+      "close",
+    ],
+  });
+
+  $('[data-fancybox="avatar"]').fancybox({
+    animationEffect: "zoom-in-out",
+    animationDuration: 600,
+    transitionDuration: 1200,
+    buttons: [
+      "zoom",
+      "close"
+    ],
+  });
+
+  $('[data-fancybox="recommendation"]').fancybox({
+    animationEffect: "zoom-in-out",
+    animationDuration: 600,
+    transitionDuration: 1200,
+    buttons: [
+      "zoom",
+      "slideShow",
+      "thumbs",
+      "close",
+    ],
+  });
+
+  $.fancybox.defaults.hash = false;
+
+  $('.current-menu-item a').clone().appendTo('.art-current-page');
+
+  $('.art-map-overlay').on('click', function() {
+    $(this).addClass('art-active');
+  });
+
+  $('.art-info-bar-btn').on('click', function() {
+    $('.art-info-bar').toggleClass('art-active');
+    $('.art-menu-bar-btn').toggleClass('art-disabled');
+  });
+
+  $('.art-menu-bar-btn').on('click', function() {
+    $('.art-menu-bar-btn , .art-menu-bar').toggleClass("art-active");
+    $('.art-info-bar-btn').toggleClass('art-disabled');
+  });
+
+  $('.art-info-bar-btn , .art-menu-bar-btn').on('click', function() {
+    $('.art-content').toggleClass('art-active');
+  });
+
+  $('.art-curtain , .art-mobile-top-bar').on('click', function() {
+    $('.art-menu-bar-btn , .art-menu-bar , .art-info-bar , .art-content , .art-menu-bar-btn , .art-info-bar-btn').removeClass('art-active , art-disabled');
+  });
+
+  $('.menu-item').on('click', function() {
+    if ($(this).hasClass('menu-item-has-children')) {
+      $(this).children('.sub-menu').toggleClass('art-active');
+    } else {
+      $('.art-menu-bar-btn , .art-menu-bar , .art-info-bar , .art-content , .art-menu-bar-btn , .art-info-bar-btn').removeClass('art-active , art-disabled');
+    }
+  });
+
