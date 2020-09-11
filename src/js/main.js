12a52ca47b455d1a2c0a60ab89b2d4ba8b5fa76c
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2020-09-11 23:46:13 +0100

    js progressbar animate

--- src/js/main.js
+++ src/js/main.js
@@ -57,3 +57,84 @@ $(function() {
         }
       });
     });
+    var bar = new ProgressBar.Line(preloader, {
+        strokeWidth: 1.7,
+        easing: 'easeInOut',
+        duration: 1400,
+        delay: 750,
+        trailWidth: 1.7,
+        svgStyle: {
+          width: '100%',
+          height: '100%'
+        },
+        step: (state, bar) => {
+          bar.setText(Math.round(bar.value() * 100) + ' %');
+        }
+      });
+    
+      bar.animate(1);
+    
+      // counters
+      anime({
+        targets: '.art-counter-frame',
+        opacity: [0, 1],
+        duration: 800,
+        delay: 2300,
+        easing: 'linear',
+      });
+    
+      anime({
+        targets: '.art-counter',
+        delay: 1300,
+        opacity: [1, 1],
+        complete: function(anim) {
+          $('.art-counter').each(function() {
+            $(this).prop('Counter', 0).animate({
+              Counter: $(this).text()
+            }, {
+              duration: 2000,
+              easing: 'linear',
+              step: function(now) {
+                $(this).text(Math.ceil(now));
+              }
+            });
+          });
+        }
+      });
+    
+      // progressbars
+      var bar = new ProgressBar.Circle(circleprog1, {
+        strokeWidth: 7,
+        easing: 'easeInOut',
+        duration: 1400,
+        delay: 2500,
+        trailWidth: 7,
+        step: function(state, circle) {
+          var value = Math.round(circle.value() * 100);
+          if (value === 0) {
+            circle.setText('');
+          } else {
+            circle.setText(value);
+          }
+        }
+      });
+    
+      bar.animate(1);
+    
+      var bar = new ProgressBar.Circle(circleprog2, {
+        strokeWidth: 7,
+        easing: 'easeInOut',
+        duration: 1400,
+        delay: 2600,
+        trailWidth: 7,
+        step: function(state, circle) {
+          var value = Math.round(circle.value() * 100);
+          if (value === 0) {
+            circle.setText('');
+          } else {
+            circle.setText(value);
+          }
+        }
+      });
+    
+      bar.animate(0.9);
